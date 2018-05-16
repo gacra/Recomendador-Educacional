31b390facfd08dcc581d849be5aebb7c465dcc62
guilherme.ja <guilherme.acra@usp.br>
2018-05-16 20:11:55 -0300

    Adicionado arquivos que não foram versionados

---
+++ Utils/rec_edu_utils/models/question.py
@@ -0,0 +1,11 @@
+from rec_edu_utils.models import Item, Field
+
+
+class Question(Item):
+
+    _id = Field()
+    stem = Field()
+    alternatives = Field()
+    correct_alt = Field()
+    topic = Field()
+    terms = Field()
---
+++ Utils/rec_edu_utils/models/topics.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Topics(Enum):
+    TIPOS_DADOS = 'Tipos de dados em C'
+    # COMANDOS_REPETICAO = 'Comandos de repetição em C'
+    # PONTEIROS = 'Ponteiros em C'
+    FUNCOES = 'Funções em C'
+    # RECURSAO = 'Recursão em C'
